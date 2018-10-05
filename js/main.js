11c2358dd6d73f38b06af48bbc6b6653bfcc18ab
Kurvani <kkeyvan1@asu.edu>
2018-10-04 23:44:36 -0700

    had function countown instead of "countdown"

--- js/main.js
+++ js/main.js
@@ -33,7 +33,44 @@ const words = [
   "javascript"
 ];
 
-//an init function Initialize game
+//an init function/ Initialize game
 function init() {
-  console.log("init");
+  // Load word from array
+  showWord(words);
+  //Call countdown every second
+  //To do this we set an interval to repeat every 1 second, we use 1000 miliseconds to do this w/ JS
+  setInterval(countdown, 1000);
+  //Function to check Game status
+  setInterval(checkStatus, 50);
+}
+
+// Pick & show random word from array
+function showWord(words) {
+  //a variable to grab a number math.foor to round down, math.random to RNG, words.length for length of words array
+  //Generate random array index
+  const randIndex = Math.floor(Math.random() * words.length);
+  // Output random word
+  //We initialized currentWord at start of JS file and use innerHTML to grab value from html h2
+  currentWord.innerHTML = words[randIndex];
+}
+
+// Countdown timer function
+function countdown() {
+  // Make sure time is not running out
+  if (time > 0) {
+    // Decrement time
+    time--;
+  } else if (time === 0) {
+    // Game is over because timer hit 0
+    isPlaying = false;
+  }
+  // Show time
+  timeDisplay.innerHTML = time;
+}
+
+//Check game status
+function checkStatus() {
+  if (!isPlaying && time === 0) {
+    message.innerHTML = "Game Over!!!";
+  }
 }
